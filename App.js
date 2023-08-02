111ca7f7393216edd124ed14e4c8b5373d5cba3a
abuyusr <abdizm@outlook.com>
2023-08-02 16:11:52 -0400

    Communication Features added

--- App.js
+++ App.js
@@ -1,32 +1,28 @@
-import { useNetInfo } from '@react-native-community/netinfo';
-import { useEffect } from 'react';
-
-import { StyleSheet, Alert } from 'react-native';
 // import the screens
 import Start from './components/Start';
 import Chat from './components/Chat';
+
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// import functions for initilaizing firebase
+import { initializeApp } from 'firebase/app';
+import { getFirestore, disableNetwork, enableNetwork } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { useNetInfo } from '@react-native-community/netinfo';
+import { useEffect } from 'react';
+
+import { Alert, LogBox } from 'react-native';
+
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
-// iniltialize a connection to firebase
-import { initializeApp } from 'firebase/app';
-import { getFirestore, disableNetwork, enableNetwork } from 'firebase/firestore';
-
-
+// Create the app component
 const App = () => {
   const connectionStatus = useNetInfo();
-
-  useEffect(() => {
-    if (connectionStatus.isConnected === false) {
-      Alert.alert("No internet connection");
-    disableNetwork(db);
-  } else if (connectionStatus.isConnected === true) {
-    enableNetwork(db);
-  }
-  }, [connectionStatus.isConnected]);
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -38,9 +34,25 @@ const firebaseConfig = {
   messagingSenderId: "761041089695",
   appId: "1:761041089695:web:1ec2b7a797cfd54e1e5eaa"
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore Database Handler
 const db = getFirestore(app);
+
+// Initialize Firebase Storage Handler
+const storage = getStorage(app);
+
+useEffect(() => {
+  if (connectionStatus.isConnected === false) {
+    Alert.alert("No internet connection");
+  disableNetwork(db);
+} else if (connectionStatus.isConnected === true) {
+  enableNetwork(db);
+}
+}, [connectionStatus.isConnected]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
@@ -49,18 +61,12 @@ const db = getFirestore(app);
           component={Start}
         />
         <Stack.Screen name="Chat">
-          {(props) => <Chat isConnected={connectionStatus.isConnected} {...props} db={db} />}
+          {(props) => <Chat isConnected={connectionStatus.isConnected} 
+           db={db} storage={storage} {...props}/>}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
 export default App;
--- components/Chat.js
+++ components/Chat.js
@@ -3,22 +3,24 @@ import { useEffect, useState } from 'react';
 import { Bubble, GiftedChat, InputToolbar } from 'react-native-gifted-chat';
 import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
 import { collection, addDoc, onSnapshot, query, orderBy, Timestamp } from "firebase/firestore";
+import CustomActions from './CustomActions';
+import MapView from 'react-native-maps';
 
-const Chat = ({ db, route, navigation, isConnected }) => {
+const Chat = ({ db, route, navigation, isConnected, storage }) => {
   const { name, color, userID } = route.params;
   const [messages, setMessages] = useState([]);
 
   let unsubMessages;
 
   useEffect(() => {
+    navigation.setOptions({ title: name });
+
     if (isConnected === true ) {
       // unregister current onSnapshot() listener to avoid registering multiple listeners when
       // useEffect code is re-executed.
       if (unsubMessages) unsubMessages();
       unsubMessages = null;
 
-    navigation.setOptions({ title: name });
-    
     const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
     unsubMessages = onSnapshot(q, (docs) => {
       let newMessages = [];
@@ -29,77 +31,98 @@ const Chat = ({ db, route, navigation, isConnected }) => {
           createdAt: new Date(doc.data().createdAt.toMillis())
         })
       })
-      cacheMessages(newMessages);
+        cacheMessages(newMessages);
         setMessages(newMessages);
     })
   } else loadCachedMessages();
-  //clean up code
-  return () => {
-    if (unsubMessages) unsubMessages();
-  }
- }, [isConnected]);
- const loadCachedMessages = async () => {
-  const cachedMessages = await AsyncStorage.getItem('messages') || [];
-  setMessages(JSON.parse(cachedMessages));
-}
 
- const cacheMessages = async (messagesToCache) => {
-  try {
-    await AsyncStorage.setItem('messages', JSON.stringify(messagesToCache));
-  } catch (error) {
-    console.error(error.message);
+    return () => {
+      if (unsubMessages) unsubMessages();
+    }
+   }, [isConnected]);
+   const loadCachedMessages = async () => {
+    const cachedMessages = await AsyncStorage.getItem('messages') || [];
+    setMessages(JSON.parse(cachedMessages));
   }
-}
- const onSend = async (newMessages) => {
+   const cacheMessages = async (messagesToCache) => {
+    try {
+      await AsyncStorage.setItem('messages', JSON.stringify(messagesToCache));
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  
+   const onSend = async (newMessages) => {
     addDoc(collection(db, "messages"), newMessages[0])
   }
 
-  const renderInputToolbar = (props) => {
-    if (isConnected === true) return <InputToolbar {...props} />;
-    else return null;
-  }
-  
-  const renderBubble = (props) => {
-    return <Bubble
-    {...props}
-    wrapperStyle={{
-      right: {
-        backgroundColor: '#000'
-      },
-      left: {
-        backgroundColor: '#fff'
+    const renderInputToolbar = (props) => {
+      if (isConnected === true) return <InputToolbar {...props} />;
+      else return null;
+    }
+
+    const renderBubble = (props) => {
+      return <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          },
+          left: {
+            backgroundColor: '#fff'
+          }
+        }}
+      />
+    }
+
+    const renderCustomActions = (props) => {
+      return <CustomActions userID={userID} storage={storage} {...props} />;
+    };
+
+    const renderCustomView = (props) => {
+      const { currentMessage} = props;
+      if (currentMessage.location) {
+        return (
+            <MapView
+              style={{width: 150,
+                height: 100,
+                borderRadius: 13,
+                margin: 3}}
+              region={{
+                latitude: currentMessage.location.latitude,
+                longitude: currentMessage.location.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }}
+            />
+        );
       }
-    }}
-  />
-}
-useEffect(() => {
-  navigation.setOptions({ title: name });
-}, []);
+      return null;
+    }
 
-return (
-<View style={{flex: 1, backgroundColor: color}}>
 
-<GiftedChat
+ return (
+  <View style={{flex: 1, backgroundColor: color}}>
+    <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
         renderInputToolbar={renderInputToolbar}
         onSend={(messages) => onSend(messages)}
+        renderActions={renderCustomActions}
+        renderCustomView={renderCustomView}
         user={{
           _id: userID, 
           name: name,
         }}
-        />
+      />
       </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
- });
- export default Chat;
-
-
+ container: {
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center'
+ }
+});
+export default Chat;
